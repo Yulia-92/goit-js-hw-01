7d5decc006adfb934e1de09fcf7edb23aafd7d20
Yulia <borisovayulia2013@gmail.com>
2024-12-07 20:14:06 +0200

    index

--- js/task-2.js
+++ js/task-2.js
@@ -4,6 +4,6 @@ function getShippingMessage(country, price, deliveryFee) {
     return product;
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20)); 
+console.log(getShippingMessage('Australia', 120, 50)); 
+console.log(getShippingMessage('Germany', 80, 20)); 
+console.log(getShippingMessage('Sweden', 100, 20)); 
